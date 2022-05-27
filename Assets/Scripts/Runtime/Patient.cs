34fb6a5ff467bc8754c2d82fe45d14c71b53236a
BamdaXP <wanxuanjun@sina.cn>
2022-05-27 21:58:28 +0800

    data

---
+++ Assets/Scripts/Runtime/Patient.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+namespace Assets.Scripts.Runtime
+{
+    [CreateAssetMenu(fileName = "Patient",menuName = "Patient")]
+    public class Patient : ScriptableObject
+    {
+
+        public float height;
+        public float[] heightRanges = new float[4];
+
+        public float depth;
+        public float[] depthRanges = new float[2];
+    }
+}
