db36819c7045ef0aa33772a37b316e6071347da4
BamdaXP <wanxuanjun@sina.cn>
2022-05-28 18:30:27 +0800

    merge

--- Assets/Scripts/Runtime/GameManager.cs
+++ Assets/Scripts/Runtime/GameManager.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 using DG.Tweening;
 public class GameManager : Singleton<GameManager>
 {
+    public enum GameState
+    {
+        Title,
+        First
+    }
     // Start is called before the first frame update
     void Start()
     {
