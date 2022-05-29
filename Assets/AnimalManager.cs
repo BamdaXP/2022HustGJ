cac3538e46ffabddec581d69b356a6d8aead0faa
SyanHsu <syanhsu@qq.com>
2022-05-30 00:08:43 +0800

    0

--- Assets/AnimalManager.cs
+++ Assets/AnimalManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
-public class AnimalManager : MonoBehaviour
+public class AnimalManager : Singleton<AnimalManager>
 {
     public List<AnimalData> datas;
     //public List<Animal> animals;
