91d2d882651cbe49c62c8564138da21fd961759d
BamdaXP <wanxuanjun@sina.cn>
2022-05-30 00:07:58 +0800

    tmp

--- Assets/AnimalManager.cs
+++ Assets/AnimalManager.cs
@@ -31,19 +31,19 @@ public class AnimalManager : MonoBehaviour
         endAnimal = testAnimal;
         if (endAnimal != null)
         {
+            endAnimal.ChangeSprite(true);
+            endAnimal.GetComponent<SpriteRenderer>().sortingLayerName = "CheckedAnimal";
             endAnimal.transform.DOMove(endPoint.position, 3);
-            endAnimal.transform.DOScaleY(1.05f, 0.3f).SetLoops(10,LoopType.Yoyo);
+            endAnimal.transform.DOScaleY(1.05f, 0.3f).SetLoops(10, LoopType.Yoyo);
         }
-            
-        
+
         testAnimal = backupAnimal;
         if (testAnimal != null)
         {
             testAnimal.transform.DOMove(testPoint.position, 3);
             testAnimal.transform.DOScaleY(1.05f, 0.3f).SetLoops(10, LoopType.Yoyo);
+            StartCoroutine(SetSpriteAfterTime(testAnimal, false, 3));
         }
-            
-
 
         backupAnimal = initAnimal;
         if (backupAnimal != null)
@@ -51,7 +51,6 @@ public class AnimalManager : MonoBehaviour
             backupAnimal.transform.DOMove(backupPoint.position, 3);
             backupAnimal.transform.DOScaleY(1.05f, 0.3f).SetLoops(10, LoopType.Yoyo);
         }
-            
 
         if (datas.Count > 0)
         {
@@ -65,5 +64,11 @@ public class AnimalManager : MonoBehaviour
             initAnimal = null;
         }
     }
-    
+
+    private IEnumerator SetSpriteAfterTime(Animal animal, bool mask, float time)
+    {
+        yield return new WaitForSeconds(time);
+        animal.ChangeSprite(mask);
+    }
 }
+
